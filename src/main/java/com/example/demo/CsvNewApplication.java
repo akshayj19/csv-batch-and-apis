bc000ee77adc735517ef2fe459497bd009818f06
Akshay kumar jain <akshayj19@gmail.com>
2022-06-14 21:02:45 +0900

    second changes

--- src/main/java/com/example/demo/CsvNewApplication.java
+++ src/main/java/com/example/demo/CsvNewApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CsvNewApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(CsvNewApplication.class, args);
 	}
 
